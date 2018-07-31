3e478f9f1e6c4ef907c8f03229d21db4f4cfca90
Evan Mei <evanme87@gmail.com>
2018-07-31 15:48:59 -0700

    Created a dictionary to store the saved thread info and exported to csv

--- get_reddit_saved.py
+++ get_reddit_saved.py
@@ -9,9 +9,27 @@ user_agent='Save_my_Saves',
 username='Terror_Rabbit', 
 password='cApriItaly878')
 
-saved = reddit.user.me().saved(limit=None)
+saved = reddit.user.me().saved(limit=3)
+thread_info_dict = {"title": [], "thumbnail": [], "subreddit": [], "score": [], "permalink": [], "num_comments": []}
 
 for item in saved:
     if isinstance(item, Submission):
-        print(item.title. item.id)
-        
+        #permalink(gives you the reddit link), thumbnail, title , score, unsave
+        thread_info_dict["title"].append(item.title)
+        thread_info_dict["thumbnail"].append(item.thumbnail)
+        thread_info_dict["subreddit"].append(item.subreddit)
+        thread_info_dict["score"].append(item.score)
+        thread_info_dict["permalink"].append(item.permalink)
+        thread_info_dict["num_comments"].append(item.num_comments)
+        print(item.title)
+    else:
+        #comments have no title or thumbnail
+        thread_info_dict["title"].append(item.body)
+        thread_info_dict["thumbnail"].append("Is Comment") 
+        thread_info_dict["subreddit"].append(item.subreddit)
+        thread_info_dict["score"].append(item.score)
+        thread_info_dict["permalink"].append(item.permalink)
+        thread_info_dict["num_comments"].append(item.num_comments)
+        print(item.body, item.name, item.permalink, item.score, item.subreddit)
+saved_data = pd.DataFrame(thread_info_dict)
+saved_data.to_csv('reddit_saved.csv')
